11052476f84a481238bae1203c5de502a43b9b17
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-05-06 16:13:42 +0900

    axios1-end

--- src/main.js
+++ src/main.js
@@ -1,7 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import axios from 'axios';
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-http-95883/databases/(default)/documents";
+
+const interceptorsRequest = axios.interceptors.request.use(
+  config => {
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+const interceptorsResponse = axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+axios.interceptors.request.eject(interceptorsRequest);
+axios.interceptors.response.eject(interceptorsResponse);
 
 new Vue({
   render: h => h(App),
